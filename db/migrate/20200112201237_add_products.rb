7e8f83fa04780f448fbcc6dad290fd1612d96ee7
Oleg Voloshyn <sparco2405@gmail.com>
2020-01-15 10:36:29 +0200

    Display products step 3, change image from salami to margarita

--- db/migrate/20200112201237_add_products.rb
+++ db/migrate/20200112201237_add_products.rb
@@ -5,6 +5,6 @@ class AddProducts < ActiveRecord::Migration[6.0]
 
   	Product.create 	({:title => 'Peperoni', :description => 'This is amaizing Peperoni pizza', :price => 450,:size => 30,	:is_spicy => false,	:is_veg => false, :is_best_offer => true, :path_to_image => '/images/peper.jpg'})
 
-  	Product.create	({:title => 'Salami', :description => 'This is nice salami pizza', :price => 400,	:size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/salam.jpg'})
+  	Product.create	({:title => 'Margarita', :description => 'This is nice Cheese pizza', :price => 400,	:size => 30, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/margarita.jpg'})
   end
 end
