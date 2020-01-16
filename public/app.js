508f33deb4a1e7dd285951654c4bf50ad5d1c652
Oleg Voloshyn <sparco2405@gmail.com>
2020-01-17 00:19:31 +0200

    Add function cart_get_number_of_items

--- public/app.js
+++ public/app.js
@@ -7,12 +7,32 @@ function something()
 	window.localStorage.setItem('bbb', x); // hh['bbb'] = x
 	alert(x);
 }
-	
+// В папке lesson 33 тестовое приложение на руби чтобы понимать как работаем с хэшем
 function add_to_cart(id) {
+
+	var key = 'product_' + id
 	
-	var x = window.localStorage.getItem('product_' + id);
+	var x = window.localStorage.getItem(key);
 	x = x*1 + 1; // x = x + 1
+	window.localStorage.setItem(key, x);
 
-	window.localStorage.setItem('product_' + id, x);
+	// Вывод количества штук в корзине
+	alert('Items in your cart: ' + cart_get_number_of_items());
+}
+function cart_get_number_of_items()
+{
+	var cnt = 0;
 
+	for (var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в руби hh['key'] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
 }
